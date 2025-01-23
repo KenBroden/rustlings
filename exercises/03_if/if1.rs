4c147a3e6c0ef64319ba8383fa4d3a67b9003944
Ken <brod0268@morris.umn.edu>
2025-01-22 23:46:53 -0600

    Added if else statement to bigger fn
    Also added implementation of bigger() to main

--- exercises/03_if/if1.rs
+++ exercises/03_if/if1.rs
@@ -4,10 +4,19 @@ fn bigger(a: i32, b: i32) -> i32 {
     // Do not use:
     // - another function call
     // - additional variables
+
+    if a > b {
+        a
+    } else {
+        b
+    }
 }
 
 fn main() {
-    // You can optionally experiment here.
+    // Input value to see fn bigger(a,b) in action
+    let a = 10;
+    let b = 8;
+    println!("The bigger number is: {}", bigger(a, b));
 }
 
 // Don't mind this for now :)
